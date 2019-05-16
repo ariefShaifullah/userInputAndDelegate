83bf720d7f6edb2f6495e2b3a60731216dfd15a9
Arief Shaifullah Akbar <shaifullah.akbar@gmail.com>
2019-05-16 10:19:02 +0700

    Initial Commit

---
+++ userInputAndDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  userInputAndDelegate
+//
+//  Created by Arief Shaifullah Akbar on 16/05/19.
+//  Copyright © 2019 Arief Shaifullah Akbar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
